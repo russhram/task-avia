bc0f6cfed571b1e7c4c8e170e2040aef68215774
ruslanshakirov <ruslan.shakirov@xored.com>
2018-04-27 18:36:05 +0700

    (#task2) Add logo and move request data to App js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import React, {Component, Fragment} from 'react';
 import {BrowserRouter as Router} from "react-router-dom";
-
-import './App.css';
+import { withState, lifecycle, compose, withHandlers } from 'recompose';
+import classNames from 'classnames';
 
 import Sidebar from './components/Sidebar';
 import Tickets from './components/Tickets';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Fragment>
-          <Sidebar/>
-          <Tickets/>
-        </Fragment>
-      </Router>
-    );
-  }
-}
+import './App.css';
+
+const enhance = compose(
+  withState('tickets', 'setTickets', []),
+  withState('rates', 'setRates', {}),
+  withState('isLoading', 'setLoading', true),
+  withHandlers({
+    callApi: props => async url => {
+      props.setLoading(true);
+      const response = await fetch(url);
+      const result = await response.json();
+      props.setLoading(false);
+      return result;
+    },
+  }),
+  withHandlers({
+    requestData: props => () => {
+      props.callApi('http://localhost:3000/tickets').then(({tickets}) => {
+        props.setTickets(tickets)
+      });
+      props.callApi('http://data.fixer.io/api/latest?access_key=80db75b40686aafe8ba34708ac6f45ba').then(({rates}) => {
+        props.setRates(rates);
+      })
+    }
+  }),
+  lifecycle({
+    componentDidMount() {
+      this.props.requestData(this.props);
+    }
+  })
+);
+
+const App = enhance(({tickets, rates, isLoading, requestData, ...props}) => (
+  <Router>
+    <Fragment>
+      <div
+        className={classNames('logo', {'logo_spin': isLoading})}
+        onClick={() => requestData(props)}
+      />
+      <div className="content">
+        <Sidebar/>
+        <Tickets tickets={tickets} rates={rates}/>
+      </div>
+    </Fragment>
+  </Router>
+));
 
 export default App;
--- src/components/Tickets/index.js
+++ src/components/Tickets/index.js
@@ -1,29 +1,7 @@
 import React from 'react';
-import { withState, lifecycle, compose } from 'recompose';
 import { withRouter } from 'react-router';
 import Ticket from './Ticket';
 import {parseCurrencyFromQS, parseStopsFromQS} from "../queryStringUtils";
-
-const callApi = async (url) => {
-  const response = await fetch(url);
-  return await response.json();
-};
-
-const enhance = compose(
-  withRouter,
-  withState('tickets', 'setTickets', []),
-  withState('rates', 'setRates', {}),
-  lifecycle({
-    componentDidMount() {
-      callApi('http://localhost:3000/tickets').then(({tickets}) => {
-        this.props.setTickets(tickets)
-      });
-      callApi('http://data.fixer.io/api/latest?access_key=80db75b40686aafe8ba34708ac6f45ba').then(({rates}) => {
-        this.props.setRates(rates);
-      })
-    }
-  })
-);
 
 const Tickets = ({tickets, rates, location}) => {
   const stops = parseStopsFromQS(location.search);
@@ -43,4 +21,4 @@ const Tickets = ({tickets, rates, location}) => {
   );
 };
 
-export default enhance(Tickets);
+export default withRouter(Tickets);
